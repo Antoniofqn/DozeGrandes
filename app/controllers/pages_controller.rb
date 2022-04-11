db2668f0aecdb30bf2890d3bd928ad42978c5ea2
Antonio Florencio <antoniofqn@hotmail.com>
2022-04-10 22:57:24 -0300

    added table html and refactored code that was in the view into the controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,11 @@
 class PagesController < ApplicationController
   def home
-    @matches = Match.all
+    if params['search'].present?
+      @home = Club.find_by(name: params['search'][:home])
+      @away = Club.find_by(name: params['search'][:away])
+      @matches = Match.where(home: [@home, @away], away: [@home, @away])
+    else
+      @matches = Match.all
+    end
   end
 end
