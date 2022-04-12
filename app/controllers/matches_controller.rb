f03a1c86c4ee879844e65a13bf6775567f6eb99b
Antonio Florencio <antoniofqn@hotmail.com>
2022-04-12 14:41:49 -0300

    added filter logic to matches

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -3,7 +3,7 @@ class MatchesController < ApplicationController
     if params['search'].present?
       @home = Club.find_by(name: params['search'][:home])
       @away = Club.find_by(name: params['search'][:away])
-      @matches = Match.where(home: [@home, @away], away: [@home, @away])
+      @matches = Match.filter(params['search'].slice(:competition, :stadium)).where(home: [@home, @away], away: [@home, @away])
     else
       @matches = Match.all
     end
--- app/models/match.rb
+++ app/models/match.rb
@@ -1,4 +1,8 @@
 class Match < ApplicationRecord
+  include Filterable
+  scope :filter_by_competition, -> (competition) { where competition: competition }
+  scope :filter_by_stadium, -> (stadium) { where stadium: stadium }
+  scope :filter_by_starts_with, -> (name) { where("name like ?", "#{name}%")}
   belongs_to :home, class_name: 'Club'
   belongs_to :away, class_name: 'Club'
 end
