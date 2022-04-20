3d64dba953d1a6a5cf126902107738349ffe20d2
Antonio Florencio <antoniofqn@hotmail.com>
2022-04-20 17:28:39 -0300

    added graph for goals scored totals

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -4,8 +4,29 @@ class MatchesController < ApplicationController
       @home = Club.find_by(name: params['search'][:home])
       @away = Club.find_by(name: params['search'][:away])
       @matches = Match.filter(params['search'].slice(:competition, :stadium, "start_date(1i)", "end_date(1i)")).where(home: [@home, @away], away: [@home, @away])
+      @stadiums = stadium_selection(@matches)
+      @competitions = competition_selection(@matches)
     else
       @matches = Match.all
     end
   end
+
+  private
+
+  def stadium_selection(matches)
+    stadiums = []
+    matches.each do |match|
+      stadiums << match.stadium unless match.stadium.in?(stadiums)
+    end
+    stadiums
+  end
+
+  def competition_selection(matches)
+    competitions = []
+    matches.each do |match|
+      competitions << match.competition unless match.competition.in?(competitions)
+    end
+    competitions
+  end
+
 end
--- app/models/club.rb
+++ app/models/club.rb
@@ -4,4 +4,15 @@ class Club < ApplicationRecord
   has_many :away_matches, class_name: 'Match',
                             foreign_key: 'away_id'
   has_one_attached :photo
+
+  def total_goals(matches)
+    total_goals = 0
+    matches.where(home: self).each do |match|
+      total_goals += match.score_home
+    end
+    matches.where(away: self).each do |match|
+      total_goals += match.score_away
+    end
+    total_goals
+  end
 end
