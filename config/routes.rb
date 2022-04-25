26b6fd4fdbe927a0ada4467e76a9e813e595143d
Antonio Florencio <antoniofqn@hotmail.com>
2022-04-24 22:57:44 -0300

    added About Us page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'matches/index'
   root to: 'matches#index'
+  get 'quem_somos', to: 'pages#quem_somos'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
